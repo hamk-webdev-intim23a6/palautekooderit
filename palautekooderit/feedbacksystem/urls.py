2862e506efe230c4e92117757f5c70003a5fe8c6
Eetu Lähdeaho <lahdeahoeetu@gmail.com>
2025-04-27 11:34:11 +0300

    Added feedback functionality, home-page forms, corrected urls

--- palautekooderit/feedbacksystem/urls.py
+++ palautekooderit/feedbacksystem/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('home/', views.home, name='home'),
     path('submit/', views.submit_feedback, name='submit_feedback'),
     path('topic/', views.topic, name='topic'),
     path('analytics/', views.analytics, name='analytics')
--- palautekooderit/feedbacksystem/views.py
+++ palautekooderit/feedbacksystem/views.py
@@ -1,15 +1,47 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Feedback, Topic
 from .forms import TopicForm
 from .forms import Topic
 
 # Allows selection of feedback item
 def home(request):
-    return HttpResponse("Select Feedback-item")
+    topic_form = TopicForm()
+    return render(request, 'home.html', {'topic_form': topic_form})
 
 #Allows giving feedback
+@login_required
 def submit_feedback(request):
-    return HttpResponse("Submit Feedback")
+    if request.method == 'POST':
+        topic_form = TopicForm(request.POST)
+        if topic_form.is_valid():
+            topic_instance = topic_form.cleaned_data['name']
+            try:
+                rating = int(request.POST.get('rating'))
+                positive = request.POST.get('positive', '')
+                negative = request.POST.get('negative', '')
+                ideas = request.POST.get('ideas', '')
+
+                feedback = Feedback(
+                    topic=topic_instance,
+                    user=request.user,
+                    rating=rating,
+                    positive=positive,
+                    negative=negative,
+                    ideas=ideas
+                )
+                feedback.save()
+
+                messages.success(request, 'Feedback sent.')
+                return redirect('home')
+            except ValueError:
+                error_message = "Please enter a valid rating between 1 and 5."
+                return render(request, 'home.html', {'topic_form': topic_form, 'error': error_message})
+        else:
+            return render(request, 'home.html', {'topic_form': topic_form, 'form_errors': topic_form.errors})
+    else:
+        return redirect('home')
 
 #Allows creation of feedback items    
 def topic(request):
