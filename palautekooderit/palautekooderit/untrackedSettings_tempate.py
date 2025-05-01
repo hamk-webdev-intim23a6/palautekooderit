32886ba309a480f3dfac04b4cbd089248193639e
AaronKivinen <aaronk216@hotmail.com>
2025-05-02 01:45:08 +0300

    Optimized mobile view, logo created

--- palautekooderit/palautekooderit/untrackedSettings_tempate.py
+++
@@ -1,5 +0,0 @@
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET KEY HERE :)'
