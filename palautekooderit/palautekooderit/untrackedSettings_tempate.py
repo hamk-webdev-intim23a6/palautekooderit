7d2de7553a49b674a2c0ead332e6548003ee90b4
Thaumatized <veeti.junkkala@gmail.com>
2025-05-01 21:57:45 +0300

    Did done did it :)

--- palautekooderit/palautekooderit/untrackedSettings_tempate.py
+++ palautekooderit/palautekooderit/untrackedSettings_tempate.py
@@ -1,2 +1,5 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG=False
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'SECRET KEY HERE :)'
