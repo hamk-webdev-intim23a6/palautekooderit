a48331ae4360036b89bd3b44bc179f4c11a789e4
Veeti Valtari Junkkala <59640376+Thaumatized@users.noreply.github.com>
2025-05-01 22:26:41 +0300

    Separate "DEBUG" from settings.py (#17)
    * Did done did it :)
    
    * .gitignore and README.md

--- palautekooderit/palautekooderit/settings.py
+++ palautekooderit/palautekooderit/settings.py
@@ -11,8 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from .secretKey import SECRET_KEY
-import os
+from .untrackedSettings import SECRET_KEY, DEBUG
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
 
 ALLOWED_HOSTS = ["4.235.120.83", "ankkapeli.fi", "127.0.0.1", "localhost"]
 
