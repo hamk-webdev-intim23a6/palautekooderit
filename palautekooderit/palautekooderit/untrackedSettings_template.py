7fbf4c3342d4060133e6fb5542253c254b0988e8
AaronKivinen <146371928+AaronKivinen@users.noreply.github.com>
2025-05-02 13:29:25 +0300

    Mobile optimization, logo create (#18)
    * Optimized mobile view: now the mobile view works as intended. Shows only logo and hamburger-menubutton.
    
    *Created a logo (original image made by Thaumatized).
    
    *Made some minor visual changes in base.html.
    
    * Requested changes
    
    * Logo scrambling
    
    * Logo scrambling continues

--- palautekooderit/palautekooderit/untrackedSettings_template.py
+++ palautekooderit/palautekooderit/untrackedSettings_template.py
@@ -1,5 +1,5 @@
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG=False
+DEBUG=True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET KEY HERE :)'
+SECRET_KEY = "Insert secret key here"
