d6254ed8d75c4233ccf409eb288e0f559f3574f7
AaronKivinen <aaronk216@hotmail.com>
2025-05-02 13:16:19 +0300

    Requested changes

---
+++ palautekooderit/palautekooderit/untrackedSettings_template.py
@@ -0,0 +1,5 @@
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG=True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "Insert secret key here"
