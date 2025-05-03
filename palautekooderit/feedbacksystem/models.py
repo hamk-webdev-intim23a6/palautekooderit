cb40c0753a9264c075b7f852fc5cb6fdded9a7fb
Thaumatized <veeti.junkkala@gmail.com>
2025-05-03 14:26:51 +0300

    Overwire old feedback with new feedback from the same user

--- palautekooderit/feedbacksystem/models.py
+++ palautekooderit/feedbacksystem/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.db.models import Q
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 class Topic(models.Model):
     name = models.CharField(max_length=200, unique=True)
@@ -19,5 +21,20 @@ class Feedback(models.Model):
     positive = models.TextField(max_length=2500, blank=True)
     negative = models.TextField(max_length=2500, blank=True)
     ideas = models.TextField(max_length=2500, blank=True)
+
     def __str__(self):
-        return f"{self.topic} - {self.rating}"
+        return f"{self.topic} - {self.user} - {self.rating}"
+        
+    def save(self, *args, **kwargs):
+        if self.user is not None:
+            try:
+                existing = Feedback.objects.get(user=self.user, topic=self.topic)
+                if existing:
+                    # Overwrite existing instead of creating a new one
+                    self.pk = existing.pk
+
+                    self.datetime = timezone.now()  # manually update timestamp
+            except Feedback.DoesNotExist:
+                pass  # No existing feedback, proceed as usual
+
+        super().save(*args, **kwargs)
